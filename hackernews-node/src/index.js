e0823b584e42d5f567d7f7bd2c98b8b79a5b8919
Shubham Prakash <28767301+iShubhamPrakash@users.noreply.github.com>
2021-08-29 23:15:35 +0530

    Add more queries and mutations

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,21 +1,43 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone developed by Shubham`
+    info: () => `This is the API of a Hackernews Clone developed by Shubham`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
   }
-}
+};
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf-8'
+  ),
   resolvers,
-})
+});
 
-server.listen()
+server
+  .listen()
   .then(({ url }) => console.log(`The server is running on ${url}`));
